96d5bb782eaf5e1380cd4cb164241f57412542d0
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-09 11:16:52 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,21 +20,62 @@ class BaseModel:
     - to_dict(): Returns a dictionary containing instance attributes in a specified format.
     - __str__(): Returns a string representation of the instance.
 
+    Attributes:
+        my_number (int): Total count of instances created.
+
+    Methods:
+        __init__(*args, **kwargs): Initializes a BaseModel instance with a unique 'id', creation time, and updates the 'my_number' count.
+        save(): Updates the 'updated_at' attribute with the current datetime.
+        to_dict(): Converts instance attributes to a dictionary format.
+        __str__(): Returns a string representation of the instance.
+
     """
     my_number = 0
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a BaseModel instance with a unique 'id', creation time,
         and updates the 'my_number' count.
 
-        """
-        self.id = uuid4()
-        self.name = None
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
 
+        Keyword Args:
+            id (str): Unique identifier for the instance.
+            created_at (str, optional): String representation of creation time (format: "%Y-%m-%dT%H:%M:%S.%f").
+            updated_at (str, optional): String representation of last update time (format: "%Y-%m-%dT%H:%M:%S.%f").
+
+        """
+        # create re-usable propery for current time
         current_time = datetime.now()
-        self.created_at = current_time
-        self.updated_at = current_time
+
+        if(kwargs):
+
+            if 'id' not in kwargs.keys():
+                self.id = str(uuid4())
+
+            if 'created_at' not in kwargs.keys():
+                self.created_at = current_time
+
+            if 'updated_at' not in kwargs.keys():
+                self.updated_at = current_time
+
+            for key, val in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at':
+                    val = datetime.strptime(val,"%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'updated_at':
+                    val = datetime.fromisoformat(val, "%Y-%m-%dT%H:%M:%S.%f")
+
+                setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.name = None
+
+            self.created_at = current_time
+            self.updated_at = current_time
 
         BaseModel.my_number += 1
 
