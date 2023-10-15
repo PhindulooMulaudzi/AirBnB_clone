e5e2965a99bc413107555e091c6aac162f6862c9
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-15 10:08:40 +0200

    add all tests

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" testing User """
+import pep8
+import unittest
+from models.user import User
+
+
+class User_testing(unittest.TestCase):
+    """ check BaseModel """
+
+    def testpep8(self):
+        """ testing codestyle """
+        pepstylecode = pep8.StyleGuide(quiet=True)
+        path_user = 'models/user.py'
+        result = pepstylecode.check_files([path_user])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
