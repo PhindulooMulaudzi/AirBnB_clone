7c74f1af22be98d0b8a3d38a5a99feabcdc58d04
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-15 20:08:25 +0200

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,14 +8,14 @@ from datetime import datetime
 
 
 class BaseModel_testing(unittest.TestCase):
-    """ check BaseModel """
+    """ checking BaseModel """
 
     def testpep8(self):
         """ testing codestyle """
         pepstylecode = pep8.StyleGuide(quiet=True)
-        rest = pepstylecode.check_files(['models/base_model.py',
-                                         'models/__init__.py',
-                                         'models/engine/file_storage.py'])
+        rest = pepstylecode.checking_files(['models/base_model.py',
+                                            'models/__init__.py',
+                                            'models/engine/file_storage.py'])
         self.assertEqual(rest.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -40,8 +40,8 @@ class test_for_base_model(unittest.TestCase):
         self.assertTrue(hasattr(object_test, "updated_at"))
 
     def test_kwargs_constructor_2(self):
-        """ check id with data """
-        dictonary = {'score': 100}
+        """ checking id with data """
+        dictonary = {'score': 80}
 
         object_test = BaseModel(**dictonary)
         self.assertTrue(hasattr(object_test, 'id'))
@@ -54,37 +54,40 @@ class test_for_base_model(unittest.TestCase):
         dictonary = {'id': 'cc9909cf-a909-9b90-9999-999fd99ca9a9',
                      'created_at': '2025-06-28T14:00:00.000001',
                      '__class__': 'BaseModel',
-                     'updated_at': '2030-06-28T14:00:00.000001',
-                     'score': 100
+                     'updated_at': '2026-06-28T14:00:00.000001',
+                     'score': 80
                      }
 
         object_test = BaseModel(**dictonary)
-        out = "[{}] ({}) {}\n".format(type(object_test).__name__, object_test.id, object_test.__dict__)
+        out = "[{}] ({}) {}\n".format(type(object_test).__name__,
+                                      object_test.id, object_test.__dict__)
 
     def test_to_dict(self):
-        """ check dict """
+        """ checking dict """
         object_test = BaseModel(score=300)
         n_dict = object_test.to_dict()
 
         self.assertEqual(n_dict['id'], object_test.id)
         self.assertEqual(n_dict['score'], 300)
         self.assertEqual(n_dict['__class__'], 'BaseModel')
-        self.assertEqual(n_dict['created_at'], object_test.created_at.isoformat())
-        self.assertEqual(n_dict['updated_at'], object_test.updated_at.isoformat())
+        self.assertEqual(n_dict['created_at'],
+                         object_test.created_at.isoformat())
+        self.assertEqual(n_dict['updated_at'],
+                         object_test.updated_at.isoformat())
 
         self.assertEqual(type(n_dict['created_at']), str)
         self.assertEqual(type(n_dict['created_at']), str)
 
     def test_datetime(self):
-        """ check datatime """
+        """ checking datatime """
         bas1 = BaseModel()
         self.assertFalse(datetime.now() == bas1.created_at)
 
     def test_BaseModel(self):
-        """ check attributes values in a BaseModel """
+        """ checking attributes values in a BaseModel """
 
-        self.my_model.name = "Holbie"
-        self.my_model.my_number = 100
+        self.my_model.name = "Holberton"
+        self.my_model.my_number = 80
         self.my_model.save()
         my_model_json = self.my_model.to_dict()
 
@@ -94,16 +97,16 @@ class test_for_base_model(unittest.TestCase):
         self.assertEqual(self.my_model.id, my_model_json['id'])
 
     def test_savefirst(self):
-        """check numbers"""
+        """checking numbers"""
         with self.assertRaises(AttributeError):
             BaseModel.save([455, 323232, 2323, 2323, 23332])
 
     def test_savesecond(self):
-        """ check string """
+        """ checking string """
         with self.assertRaises(AttributeError):
-            BaseModel.save("THIS IS A TEST")
+            BaseModel.save("Test Model")
 
     def test_inst(self):
-        """check class """
+        """checkinging class """
         ml = BaseModel()
         self.assertTrue(ml, BaseModel)
